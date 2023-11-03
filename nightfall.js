9af20e8010fe94ca25c073206d43127985102067
nickievellys <nicollyoliveira419@gmail.com>
2023-11-03 17:09:57 -0400

    Create nightfall.js

---
+++ nightfall.js
@@ -0,0 +1,39 @@
+let a= 10;
+let b = 20;
+
+console.log(a+b);
+
+let c= 0.5;
+let d= 1;
+
+console.log(c+d);
+
+let e = 60;
+let f = 40;
+
+console.log (e-f);
+
+let g = 12;
+let h= 2 ;
+
+console.log (g/h);
+
+let i= 9
+let j= 8
+
+console.log (i*j);
+
+if (true) {
+    var k= 45
+}
+console.log(k);
+
+if (true) {
+    let l= 10
+    console.log(l);
+}
+
+if (true) {
+    const n=2
+console.log (n);
+}
