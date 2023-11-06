eb89302fc92015ec5483032511f1e166f6fe4be9
nickievellys <nicollyoliveira419@gmail.com>
2023-11-06 16:28:38 -0400

    chase

---
+++ aula/chase.js
@@ -0,0 +1,54 @@
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+  console.log(num1 + " é maior que " + num2);
+} else if (num2 > num1) {
+  console.log(num2 + " é maior que " + num1);
+} else {
+  console.log("Os números são iguais");
+}
+
+
+
+var idade = 18;
+if (idade >= 18) {
+console.log("Você é maior de idade.");
+}
+var idade = 14;
+if (idade >= 18) {
+console.log("Você é maior de idade.");
+} else {
+console.log("Você é menor de idade.");
+} 
+
+let numero = 10
+
+if(numero > 0) {
+  console.log("O número é positivo");
+} else if(numero < 0) {
+  console.log("O número é negativo");
+} else {
+  console.log("O número é zero");
+}
+
+function celsiusToFahrenheit(celsius) {
+    var fahrenheit = (celsius * 6/5) + 32;
+    return fahrenheit;
+  }
+  
+  var temperaturaCelsius = 45;
+  var temperaturaFahrenheit = celsiusToFahrenheit(temperaturaCelsius);
+  
+  console.log(temperaturaFahrenheit);
+
+
+  
+let base = 10;
+let altura = 5;
+
+let area = base * altura;
+
+console.log("A área do retângulo é: " + area);
+
+
